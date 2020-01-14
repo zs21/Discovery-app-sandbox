2ac60ddf47157e02a785307e30f4e6671d2443bb
Žygimantas <58956036+ztothes@users.noreply.github.com>
2020-01-14 20:58:47 +0200

    first commit
    made first commit for a test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h3> it's magic!</h3>
     </div>
   );
 }
